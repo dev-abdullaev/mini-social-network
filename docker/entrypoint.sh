b1e6d7de5f9ce8ea50f3d7f0855bd69fa8a8899d
dev-abdullaev <developer.abdullaev@gmail.com>
2026-07-06 15:25:31 +0500

    fix: celery containers wait for migrations on cold start

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -5,4 +5,11 @@ if [ "$1" = "gunicorn" ]; then
   python manage.py migrate --noinput
 fi
 
+if [ "$1" = "celery" ]; then
+  until python manage.py migrate --check > /dev/null 2>&1; do
+    echo "Waiting for database migrations..."
+    sleep 3
+  done
+fi
+
 exec "$@"
